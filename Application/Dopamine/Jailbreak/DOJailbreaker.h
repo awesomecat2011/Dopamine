7c455bb5bc58a623e7429c6ad237bf8eea74c228
awesomecat2011 <66845682+awesomecat2011@users.noreply.github.com>
2024-08-02 10:50:09 +0200

    finalize check if _systemInfoXdict = non-null before xpc_release

--- Application/Dopamine/Jailbreak/DOJailbreaker.h
+++ Application/Dopamine/Jailbreak/DOJailbreaker.h
@@ -3,10 +3,9 @@
 //  Dopamine
 //
 //  Created by Lars Fröder on 10.01.24.
-//
+//  edited by awesomecat2011 08/02/2024
 
 #import <Foundation/Foundation.h>
-
 #import <xpc/xpc.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -22,3 +21,21 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 NS_ASSUME_NONNULL_END
+
+#import "Jailbreaker.h"
+
+@implementation DOJailbreaker
+
+- (void)runWithError:(NSError **)errOut didRemoveJailbreak:(BOOL*)didRemove showLogs:(BOOL *)showLogs {
+    // original method impl
+}
+
+- (void)finalize {
+    if (_systemInfoXdict) {
+        xpc_release(_systemInfoXdict);  // Properly release _systemInfoXdict to prevent memory leaks
+        _systemInfoXdict = NULL;
+    }
+    // original finalize impl
+}
+
+@end
