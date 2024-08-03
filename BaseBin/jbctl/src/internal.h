9b38000336b0da940c3cba51f695f74c011cbfe1
awesomecat2011 <66845682+awesomecat2011@users.noreply.github.com>
2024-08-03 05:49:55 +0200

    cpp compatibility ^_^

--- BaseBin/jbctl/src/internal.h
+++ BaseBin/jbctl/src/internal.h
@@ -1,1 +1,14 @@
-int jbctl_handle_internal(const char *command, int argc, char* argv[]);
+#ifndef INTERNAL_H
+#define INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int jbctl_handle_internal(const char *command, int argc, char *argv[]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // INTERNAL_H
